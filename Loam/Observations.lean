914a9e59f50b36c0c442d2accf492c0a41755b1c
shumoku88-bit <shumoku88@gmail.com>
2026-09-06 11:34:21 +0900

    experiment: register Observation 200

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -35,6 +35,7 @@ import Loam.Observations.Observation192
 import Loam.Observations.Observation193
 import Loam.Observations.Observation194
 import Loam.Observations.Observation195
+import Loam.Observations.Observation200
 
 /-!
 # Historical Lean observations
