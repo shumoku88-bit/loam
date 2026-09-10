0e974b95247b4285ff705f3d1d145471d3b6f77a
shumoku88-bit <shumoku88@gmail.com>
2026-09-10 23:00:54 +0900

    lean: select Observation 242 coverage witness

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -31,6 +31,7 @@ import Loam.Observations.Observation195
 import Loam.Observations.Observation196
 import Loam.Observations.Observation239
 import Loam.Observations.Observation241
+import Loam.Observations.Observation242
 import Loam.Observations.StructuralS003
 import Loam.Observations.StructuralS008
 
