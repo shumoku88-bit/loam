6ef373edf437d80994e0b51a380c3ea301fa2abe
shumoku88-bit <shumoku88@gmail.com>
2026-09-16 00:42:25 +0900

    test: include Observation 255 in selected Lean umbrella

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -33,6 +33,7 @@ import Loam.Observations.Observation239
 import Loam.Observations.Observation241
 import Loam.Observations.Observation250
 import Loam.Observations.Observation253
+import Loam.Observations.Observation255
 import Loam.Observations.StructuralS003
 import Loam.Observations.StructuralS008
 
