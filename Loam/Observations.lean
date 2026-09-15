ac45acd72d715fcc4b3ce75a423e01166545528e
shumoku88-bit <shumoku88@gmail.com>
2026-09-16 01:55:36 +0900

    experiment: select Observation 261

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -39,6 +39,7 @@ import Loam.Observations.Observation257
 import Loam.Observations.Observation258
 import Loam.Observations.Observation259
 import Loam.Observations.Observation260
+import Loam.Observations.Observation261
 import Loam.Observations.StructuralS003
 import Loam.Observations.StructuralS008
 
