5bc897c155d5abad97c44da03b3e969cf2337dd0
shumoku88-bit <shumoku88@protonmail.com>
2026-09-20 20:59:58 +0900

    experiment: add exchange boundary to observation umbrella

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -52,6 +52,7 @@ import Loam.Observations.Observation278
 import Loam.Observations.Observation279
 import Loam.Observations.Observation280
 import Loam.Observations.Observation281
+import Loam.Observations.Observation282
 import Loam.Observations.StructuralS003
 import Loam.Observations.StructuralS008
 
