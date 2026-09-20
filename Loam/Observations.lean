afb5dcb7caa3cbef31faea29131438a25ddb7dec
shumoku88-bit <shumoku88@protonmail.com>
2026-09-21 02:30:42 +0900

    test: include partial disposal provenance observation

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -59,6 +59,7 @@ import Loam.Observations.StructuralS008
 import Loam.Observations.Observation284
 import Loam.Observations.Observation285
 import Loam.Observations.Observation287
+import Loam.Observations.Observation288
 
 /-!
 # Selected live Lean observations
