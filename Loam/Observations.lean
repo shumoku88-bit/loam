731969331129cc80f21857c3eafbcea1d999f731
shumoku88-bit <shumoku88@protonmail.com>
2026-09-21 13:17:28 +0900

    merge main observation umbrella into investment pressure branch

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -58,6 +58,7 @@ import Loam.Observations.StructuralS003
 import Loam.Observations.StructuralS008
 import Loam.Observations.Observation284
 import Loam.Observations.Observation285
+import Loam.Observations.Observation286
 import Loam.Observations.Observation287
 import Loam.Observations.Observation288
 import Loam.Observations.Observation289
