8b792733a69e484966b14ff547c9bbb467072801
shumoku88-bit <shumoku88@protonmail.com>
2026-09-23 03:28:00 +0900

    build: include minimal future characterization

--- Loam/Observations.lean
+++ Loam/Observations.lean
@@ -88,6 +88,7 @@ import Loam.Observations.Observation312
 import Loam.Observations.Observation313
 import Loam.Observations.Observation314
 import Loam.Observations.Observation315
+import Loam.Observations.Observation316
 
 /-!
 # Selected live Lean observations
